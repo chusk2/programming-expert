e3f2d28dedbb3de23559f02a28ad322467b46d4e
Daniel <294.daniel@gmail.com>
2021-12-08 22:19:54 +0100

    attemps need to be fixed

--- random_number_guesser.py
+++ random_number_guesser.py
@@ -3,39 +3,48 @@ import random
 # Write your code here.
 # Make sure to use `random.randint` to pick your random number.
 while True:
-    start = input('Enter the start of the range: ')
+    
     try:
-        start = int(start)
+        start = int(input('Enter the start of the range: '))
         break
+    except ValueError:
+        print('Please enter a valid number.')
+    
+while True:
+
+    try:
+        end = int(input('Enter the end of the range: '))
+        if end >= start:
+            break
+    except ValueError:
+        print('Please enter a valid number.')
+        continue
+    # the number was integer but was lower than start
+    print('Please enter a valid number.')
+
+#number_to_guess = random.randint(start,end)
+number_to_guess = 0
+#print(number_to_guess)
+attempts = 0
+
+# start guessing
+while True:
+
+    try:
+        attempts += 1
+        guess = int(input('Guess a number: '))
+
     except ValueError:
         print('Please enter a valid number.')
         continue
     
-while True:
-    try:
-        end = int(input('Enter the end of the range: '))
-    except ValueError:
-        print('Please enter a valid number.')
-        continue
-    if end >= start:
-        break
-    print('Please enter a valid number.')
-
-number_to_guess = random.randint(start,end)
-#print(number_to_guess)
-attempts = 1
-while True:
-    try:
-        guess = int(input('Guess a number: '))
-    except ValueError:
-        
-        print('Please enter a valid number.')
-        continue
     if guess == number_to_guess:
-        print(f'You guessed the number in {attempts}', end=' ')
+        print(f'You guessed the number in {attempts} ', end='')
         if attempts == 1:
             print('attempt')
         else:
             print('attempts')
         break
-    attempts += 1
+
+    #### Do not count the last attempt if it was a guess
+    
