96b76db2717683026ae18861317543f51c2188ad
Daniel <294.daniel@gmail.com>
2021-12-09 22:55:58 +0100

    finally passed test

--- random_number_guesser.py
+++ random_number_guesser.py
@@ -3,7 +3,8 @@ import random
 # Write your code here.
 # Make sure to use `random.randint` to pick your random number.
 while True:
-    
+    ### Possible bug: negative numbers are also integers
+    ### but return False when 'negative number'.isidigit()
     start = input('Enter the start of the range: ')
     if start.isdigit():
         start = int(start)
@@ -18,29 +19,30 @@ while True:
         if int(end) >= start:
             end = int(end)
             break
+        else:
+            print('Please enter a valid number.')
+    
     else:
         print('Please enter a valid number.')
 
 number_to_guess = random.randint(start,end)
-#print(number_to_guess)
-attempts = 1
+attempts = 0
 
 # start guessing
 while True:
 
     guess = input('Guess a number: ')
-    
     if guess.isdigit():
-
+        attempts += 1
+        guess = int(guess)
         if guess == number_to_guess:
-            print(f'You guessed the number in {attempts} ', end='')
-        if attempts == 1:
-            print('attempt')
-        else:
-            print('attempts')
-        break
+            if attempts == 1:
+                suffix = ''
+            elif attempts > 1:
+                suffix = 's'
+            print(f'You guessed the number in {attempts} attempt{suffix}')              
+            break
 
     else:
         print('Please enter a valid number.')
-    attempts += 1
     
